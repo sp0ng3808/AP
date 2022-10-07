f4f19ee88a9017779e1f69e604315bfdca8b9543
sp0ng3808 <111599785+sp0ng3808@users.noreply.github.com>
2022-10-07 15:46:26 +0700

    add UI menu

--- Latihan5.2.cpp
+++ Latihan5.2.cpp
@@ -6,27 +6,26 @@ int input, jumlahBarang;
 float totalBayar, totalBayarBuku, totalBayarPensil, totalBayarPnghps, totalBayarSpidol;
 char ulangi;
 
-int harga[5] = 
-{
-        0,
-        5000,    // harga buku
-        1500,    // harga pensil
-        750,      // harga penghapus
-        10500  // harga spidol
-};
+int harga[5] = {0, 5000, 1500, 750, 10500};
 
-string tampilanHarga[5] = {
-        "",
-        "1. Buku \t: Rp. 5000",
-        "2. Pensil \t: Rp. 1.500",
-        "3. Penghapus \t: Rp. 750",
-        "4. Spidol \t: Rp. 10.500"
-};
+string tampilan[5] = {"", "1. Buku      : Rp.5000", "2. Pensil    : Rp.1.500", "3. Penghapus : Rp.750", "4. Spidol    : Rp.10.500"};
+
+void lineHorizontal() {
+        for (int i = 0; i < 31; i++)
+        {
+                cout << "\u2500";
+        }
+        cout << "\n";
+}
 
 int main()
 {
         ulangi1:
         system("clear");
+        cout << " ";
+        // lineHorizontal();
+        cout << "┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓\n";
+        cout << " ┃     Barang yang tersedia\t┃ \n";
         for (int  no = 0; no < 5; no++)
         {
                 if (no == 0) continue;
@@ -34,23 +33,26 @@ int main()
                 {
                         if (no == input) continue;
                 }
-                
-                cout << tampilanHarga[no];
+                cout << " ┃ " << tampilan[no];
+                cout << "\t┃";
                 cout << "\n";
         }
 
-        cout << "Masukan angka (1-4): ";
+        cout << " ";
+        cout << "┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛\n";
+        cout << "  Masukan angka (1-4): ";
         cin >> input;
 
         switch (input)
         {
         case 1:
-                cout << "Jumlah Barang: ";
+                cout << "  Jumlah Barang: ";
                 cin >> jumlahBarang;
 
                 totalBayarBuku = harga[input] * jumlahBarang;
 
-                cout << "Apakah ada yang masih ingin dibeli ? [Y/n]: ";
+                beliLagi:
+                cout << "  Apakah ada yang masih ingin dibeli ? [Y/n]: ";
                 cin >> ulangi;
 
                 switch (ulangi)
@@ -67,17 +69,19 @@ int main()
                                 goto selesai;
                                 break;
                         default:
+                                goto beliLagi;
                                 break;
                 }
                 break;
         
         case 2:
-                cout << "Jumlah Barang: ";
+                cout << "  Jumlah Barang: ";
                 cin >> jumlahBarang;
 
                 totalBayarPensil = harga[input] * jumlahBarang;
 
-                cout << "Apakah ada yang masih ingin dibeli ? [Y/n]: ";
+                beliLagi2:
+                cout << "  Apakah ada yang masih ingin dibeli ? [Y/n]: ";
                 cin >> ulangi;
 
                 switch (ulangi)
@@ -85,7 +89,7 @@ int main()
                         case 'y':
                         case  'Y':
                                 goto ulangi1;
-                                return totalBayarPensil;
+                                return totalBayarBuku;
                                 return input;
                                 break;
                         
@@ -94,16 +98,18 @@ int main()
                                 goto selesai;
                                 break;
                         default:
+                                goto beliLagi2;
                                 break;
                 }
                 break;
         case 3:
-                cout << "Jumlah Barang: ";
+                cout << "  Jumlah Barang: ";
                 cin >> jumlahBarang;
 
                 totalBayarPnghps = harga[input] * jumlahBarang;
 
-                cout << "Apakah ada yang masih ingin dibeli ? [Y/n]: ";
+                beliLagi3:
+                cout << "  Apakah ada yang masih ingin dibeli ? [Y/n]: ";
                 cin >> ulangi;
 
                 switch (ulangi)
@@ -111,7 +117,7 @@ int main()
                         case 'y':
                         case  'Y':
                                 goto ulangi1;
-                                return totalBayarPnghps;
+                                return totalBayarBuku;
                                 return input;
                                 break;
                         
@@ -120,24 +126,26 @@ int main()
                                 goto selesai;
                                 break;
                         default:
+                                goto beliLagi3;
                                 break;
                 }
                 break;
         case 4:
-                cout << "Jumlah Barang: ";
+                cout << "  Jumlah Barang: ";
                 cin >> jumlahBarang;
 
                 totalBayarSpidol = harga[input] * jumlahBarang;
 
-                cout << "Apakah ada yang masih ingin dibeli ? [Y/n]: ";
+                beliLagi4:
+                cout << "  Apakah ada yang masih ingin dibeli ? [Y/n]: ";
                 cin >> ulangi;
-                
+
                 switch (ulangi)
                 {
                         case 'y':
                         case  'Y':
                                 goto ulangi1;
-                                return totalBayarSpidol;
+                                return totalBayarBuku;
                                 return input;
                                 break;
                         
@@ -146,11 +154,32 @@ int main()
                                 goto selesai;
                                 break;
                         default:
+                                goto beliLagi4;
                                 break;
                 }
                 break;
         default:
-                goto ulangi1;
+                cout << "  Barang Tidak Tesedia ! \n";
+                cout << "  Ulangi ? [Y/n]: ";
+                cin >> ulangi;
+
+                switch (ulangi)
+                {
+                        case 'y':
+                        case  'Y':
+                                goto ulangi1;
+                                return totalBayarBuku;
+                                return input;
+                                break;
+                        
+                        case 'n':
+                        case  'N':
+                                exit(0);
+                                break;
+                        default:
+                                goto ulangi1;
+                                break;
+                }
                 break;
         }
 
@@ -158,7 +187,7 @@ int main()
         if (totalBayarBuku > 0 || totalBayarPensil > 0 || totalBayarPnghps > 0 || totalBayarSpidol > 0)
         {
                 totalBayar = totalBayarBuku + totalBayarPensil + totalBayarPnghps + totalBayarSpidol;
-                cout << "Total Bayar adalah Rp. " << totalBayar;
+                cout << "  Total Bayar adalah Rp. " << totalBayar;
                 cout << endl;
         }
 }
