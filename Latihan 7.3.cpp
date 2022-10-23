1e02d120793d488642c562676f41a4afca069780
sp0ng3808 <111599785+sp0ng3808@users.noreply.github.com>
2022-10-23 21:08:40 +0700

    Update Latihan 7.3.cpp

--- Latihan 7.3.cpp
+++ Latihan 7.3.cpp
@@ -3,16 +3,15 @@
 #include <iomanip>
 using namespace std;
 
-int pilih, jumlahBeli[30], diskon, jumlah, harga[50], j;
+int pilih, jumlahBeli[30], jumlah, harga[50], j;
 long int subTotal[30], jumlahHarga = 0, bayar, kembalian;
 char kodeMinuman[30];
-string namaBarang[50];
+string namaBarang[50], diskon;
 float total;
 
-void menu() {
-
+void menu() 
+{
         cout << "MENU PESANAN\n\n"; 
-
         cout << "A. Kopi Susu\n";
         cout << "B. Kopi Hitam\n";
         cout << "C. Cappucino\n"; 
@@ -23,9 +22,10 @@ void menu() {
 int main()
 {
         ulang:
+        system("clear");
         menu();
 
-        cout << "Jumlah Minuman: ";
+        cout << "Jumlah minuman yang ingin dibeli: ";
         cin >> j; 
 
         for (int i = 1; i <= j; i++)
@@ -39,60 +39,84 @@ int main()
 
         for (int i = 1; i <= j; i++)
         {
-                if (kodeMinuman[i] == 'A' || kodeMinuman[i] == 'a')
+                ulang1:
+
+                switch (kodeMinuman[i])
                 {
-                        namaBarang[i] = "Kopi Susu";
-                        harga[i] = 7500;
-                } 
-                else if (kodeMinuman[i] == 'B' || kodeMinuman[i] == 'b')
-                {
-                        namaBarang[i] = "Kopi Hitam";
-                        harga[i] = 5000;
-                } 
-                else if (kodeMinuman[i] == 'C' || kodeMinuman[i] == 'c')
-                {
-                        namaBarang[i] = "Cappucino";
-                        harga[i] = 13000;
-                }
-                else if (kodeMinuman[i] == 'D' || kodeMinuman[i] == 'd')
-                {
-                        namaBarang[i] = "Kopi Luwak";
-                        harga[i] = 15000;
-                }
-                else if (kodeMinuman[i] == 'E' || kodeMinuman[i] == 'e')
-                {
-                        namaBarang[i] = "Caffe Latte";
-                        harga[i] = 13000;
+                        case 'A':
+                        case 'a':
+                                namaBarang[i] = "Kopi Susu";
+                                harga[i] = 7500;
+                                break;
+                        case 'B':
+                        case 'b':
+                                namaBarang[i] = "Kopi Hitam";
+                                harga[i] = 5000;
+                                break;
+                        case 'C':
+                        case 'c':
+                                namaBarang[i] = "Cappucino";
+                                harga[i] = 13000;
+                                break;
+                        case 'D':
+                        case 'd':
+                                namaBarang[i] = "Kopi Luwak";
+                                harga[i] = 15000;
+                                break;
+                        case 'E':
+                        case 'e':
+                                namaBarang[i] = "Caffe Latte";
+                                harga[i] = 13000;
+                                break;
+                        default:
+                                goto ulang1;
+                                break;
                 }
         }
         
         system("clear");
-        cout << "--------------------------------------------" << endl; 
-        cout << "|   Nama Minuman    |  Jumlah  |   Harga   |" << endl;
-        cout << "--------------------------------------------" << endl;
+        cout << "---------------------------------------------------------" << endl; 
+        cout << "|   Nama Minuman    |  Jumlah  |   Diskon   |   Harga   |" << endl;
+        cout << "---------------------------------------------------------" << endl; 
 
         for (int i = 1; i <= j; i++)
         {
                 subTotal[i] = jumlahBeli[i] *  harga[i];
-                cout << "    " << namaBarang[i] << "\t     " << setiosflags(ios::right) << setw(5) << jumlahBeli[i] << "    " << setiosflags(ios::right) << setw(7) << "Rp. " << subTotal[i] << endl;
-                cout << "--------------------------------------------" << endl;
+                cout << "    " << namaBarang[i] << "\t     " << setiosflags(ios::right) << setw(5) << jumlahBeli[i] << "  " << setiosflags(ios::right) << setw(22) << "Rp." << subTotal[i] << endl;
+                cout << "---------------------------------------------------------" << endl; 
                 jumlahHarga = jumlahHarga + subTotal[i];
         }
 
-        if (jumlahHarga >= 20000)
+        if (jumlahHarga >= 40000)
         {
-                diskon =  0.05 * jumlahHarga;
-                total = jumlahHarga - diskon;
-        } else if (jumlahHarga >= 40000)
-        {
-                diskon =  0.1 * jumlahHarga;
-                total = jumlahHarga - diskon;
+                diskon =  "10%";
+                total = jumlahHarga - (0.1 * jumlahHarga);
         }
+        else if (jumlahHarga >= 20000 && jumlahHarga <= 40000)
+        {
+                diskon =  "5%";
+                total = jumlahHarga - (0.05 * jumlahHarga);
+        } 
         else {
                 total = jumlahHarga;
         }
-        
 
-        cout << total;
+        cout << "|   Total Bayar: "; 
+        cout << "\t\t    " << diskon;
+        cout << "\t       Rp." << total << endl; 
+        cout << "---------------------------------------------------------" << endl; 
+        
+        cout << "\n    Masukan uang bayar: ";
+        cin >> bayar;
+
+        if (bayar <= total)
+        {
+                cout << "    Maaf, uang anda kurang !" << endl;
+                exit(0); 
+        }
+        
+        kembalian = bayar - total;
+
+        cout << "    Kembalian anda Rp." << kembalian;
         cout << endl;
 }
